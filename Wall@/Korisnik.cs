3f939988df4f016bdf898a1c48fbf8d798a56c4a
Filip Jovanović <filip.jovanovic14@gmail.com>
2018-05-09 20:04:02 +0200

    Stvorena public klasa Korisnik
    Stvorena klasa Korisnik pomoću koje ćemo instancirati korisnike iz baze u listu za login.

---
+++ Wall@/Korisnik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Korisnik
+{
+
+    private int ID { get; set; }
+    public string Ime { get; set; }
+    public string Prezime { get; set; }
+    public string Email { get; set; }
+    private string Lozinka { get; set; }
+
+    public Korisnik(int ID, string Ime, string Prezime, string Email, string Lozinka)
+	{
+        this.ID = ID;
+        this.Ime = Ime;
+        this.Prezime = Prezime;
+        this.Email = Email;
+        this.Lozinka = Lozinka;
+	}
+}
