abf90c67bf53d136287938f05cd444f4bf1689b1
Filip Jovanović <filip.jovanovic14@gmail.com>
2018-05-10 11:21:14 +0200

    Spajanje na bazu putem DataSet-a
    Dodan DataSet "Wallet" u kojem će biti sve tablice iz baze podataka. Dostupan iz glavnog projekta u solutionu.

--- Wall@/PocetniZaslon/Properties/Settings.Designer.cs
+++ Wall@/PocetniZaslon/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PocetniZaslon.Properties
-{
-
-
+namespace PocetniZaslon.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=31.147.204.119\\PISERVER,1433;Initial Catalog=18027_DB;Persist Securit" +
+            "y Info=True;User ID=18027_User;Password=LOqkF*ak")]
+        public string _18027_DBConnectionString {
+            get {
+                return ((string)(this["_18027_DBConnectionString"]));
             }
         }
     }
