4e45ede836e0314d13c17467aa333e3a7e98a0ca
Luka Čosić <cosic.luka1@gmail.com>
2018-05-24 16:02:39 +0200

    Dorada forme "FrmGlavniIzbornik" i poboljšano otvaranje "FrmBankovniRacuni"
    Svojstvo formBorderStyle FrmBankovniRacuni stavljeno na "none".
    Postavljen je region "Upravljanje Gumbima Glavnog Izbornika" unutar FrmGlavniIzbornik za bolju organizaciju pisanja koda.
    Region se odnosi na sve radnje koje se izvode klikom na jedan od gumbiju glavnog izbornika.
    
    Napravljena metoda koja zatvara sve otvorene forme koje su unutar MDI containera forme FrmGlavniIzbornik.
    Ta metoda se poziva na početku kad korisnik klikom želi otvoriti neku formu unutar MDI containera.
    Nakon što se zatvore forme u MDI containeru otvara se odabrana forma.

--- Wall@/PocetniZaslon/FrmGlavniIzbornik.Designer.cs
+++ Wall@/PocetniZaslon/FrmGlavniIzbornik.Designer.cs
@@ -46,7 +46,7 @@
 			// 
 			this.splitterMenuScreen.Cursor = System.Windows.Forms.Cursors.Arrow;
 			this.splitterMenuScreen.Location = new System.Drawing.Point(0, 0);
-			this.splitterMenuScreen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.splitterMenuScreen.Margin = new System.Windows.Forms.Padding(2);
 			this.splitterMenuScreen.Name = "splitterMenuScreen";
 			this.splitterMenuScreen.Size = new System.Drawing.Size(194, 800);
 			this.splitterMenuScreen.TabIndex = 9;
@@ -57,7 +57,7 @@
 			this.pictureBoxLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo.BackgroundImage")));
 			this.pictureBoxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
 			this.pictureBoxLogo.Location = new System.Drawing.Point(9, 10);
-			this.pictureBoxLogo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.pictureBoxLogo.Margin = new System.Windows.Forms.Padding(2);
 			this.pictureBoxLogo.Name = "pictureBoxLogo";
 			this.pictureBoxLogo.Size = new System.Drawing.Size(175, 158);
 			this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
@@ -70,7 +70,7 @@
 			this.btnOdjava.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnOdjava.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.btnOdjava.Location = new System.Drawing.Point(9, 728);
-			this.btnOdjava.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnOdjava.Margin = new System.Windows.Forms.Padding(2);
 			this.btnOdjava.Name = "btnOdjava";
 			this.btnOdjava.Size = new System.Drawing.Size(175, 63);
 			this.btnOdjava.TabIndex = 17;
@@ -84,12 +84,13 @@
 			this.btnPostavke.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnPostavke.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.btnPostavke.Location = new System.Drawing.Point(9, 661);
-			this.btnPostavke.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnPostavke.Margin = new System.Windows.Forms.Padding(2);
 			this.btnPostavke.Name = "btnPostavke";
 			this.btnPostavke.Size = new System.Drawing.Size(175, 63);
 			this.btnPostavke.TabIndex = 16;
 			this.btnPostavke.Text = "Postavke";
 			this.btnPostavke.UseVisualStyleBackColor = false;
+			this.btnPostavke.Click += new System.EventHandler(this.btnPostavke_Click);
 			// 
 			// btnStatistika
 			// 
@@ -97,12 +98,13 @@
 			this.btnStatistika.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnStatistika.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.btnStatistika.Location = new System.Drawing.Point(9, 593);
-			this.btnStatistika.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnStatistika.Margin = new System.Windows.Forms.Padding(2);
 			this.btnStatistika.Name = "btnStatistika";
 			this.btnStatistika.Size = new System.Drawing.Size(175, 63);
 			this.btnStatistika.TabIndex = 15;
 			this.btnStatistika.Text = "Statistika";
 			this.btnStatistika.UseVisualStyleBackColor = false;
+			this.btnStatistika.Click += new System.EventHandler(this.btnStatistika_Click);
 			// 
 			// btnInvesticijskiPortfolio
 			// 
@@ -110,12 +112,13 @@
 			this.btnInvesticijskiPortfolio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnInvesticijskiPortfolio.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.btnInvesticijskiPortfolio.Location = new System.Drawing.Point(9, 526);
-			this.btnInvesticijskiPortfolio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnInvesticijskiPortfolio.Margin = new System.Windows.Forms.Padding(2);
 			this.btnInvesticijskiPortfolio.Name = "btnInvesticijskiPortfolio";
 			this.btnInvesticijskiPortfolio.Size = new System.Drawing.Size(175, 63);
 			this.btnInvesticijskiPortfolio.TabIndex = 14;
 			this.btnInvesticijskiPortfolio.Text = "Investicijski portfolio";
 			this.btnInvesticijskiPortfolio.UseVisualStyleBackColor = false;
+			this.btnInvesticijskiPortfolio.Click += new System.EventHandler(this.btnInvesticijskiPortfolio_Click);
 			// 
 			// btnPregled
 			// 
@@ -123,12 +126,13 @@
 			this.btnPregled.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnPregled.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.btnPregled.Location = new System.Drawing.Point(9, 458);
-			this.btnPregled.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnPregled.Margin = new System.Windows.Forms.Padding(2);
 			this.btnPregled.Name = "btnPregled";
 			this.btnPregled.Size = new System.Drawing.Size(175, 63);
 			this.btnPregled.TabIndex = 13;
 			this.btnPregled.Text = "Pregled transakcija";
 			this.btnPregled.UseVisualStyleBackColor = false;
+			this.btnPregled.Click += new System.EventHandler(this.btnPregled_Click);
 			// 
 			// btnUnosTransakcije
 			// 
@@ -136,12 +140,13 @@
 			this.btnUnosTransakcije.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnUnosTransakcije.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.btnUnosTransakcije.Location = new System.Drawing.Point(9, 391);
-			this.btnUnosTransakcije.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnUnosTransakcije.Margin = new System.Windows.Forms.Padding(2);
 			this.btnUnosTransakcije.Name = "btnUnosTransakcije";
 			this.btnUnosTransakcije.Size = new System.Drawing.Size(175, 63);
 			this.btnUnosTransakcije.TabIndex = 12;
 			this.btnUnosTransakcije.Text = "Unos transakcija";
 			this.btnUnosTransakcije.UseVisualStyleBackColor = false;
+			this.btnUnosTransakcije.Click += new System.EventHandler(this.btnUnosTransakcije_Click);
 			// 
 			// btnBankovniRacun
 			// 
@@ -149,7 +154,7 @@
 			this.btnBankovniRacun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnBankovniRacun.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.btnBankovniRacun.Location = new System.Drawing.Point(9, 323);
-			this.btnBankovniRacun.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnBankovniRacun.Margin = new System.Windows.Forms.Padding(2);
 			this.btnBankovniRacun.Name = "btnBankovniRacun";
 			this.btnBankovniRacun.Size = new System.Drawing.Size(175, 63);
 			this.btnBankovniRacun.TabIndex = 11;
@@ -163,12 +168,13 @@
 			this.btnZaslon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnZaslon.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.btnZaslon.Location = new System.Drawing.Point(9, 256);
-			this.btnZaslon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.btnZaslon.Margin = new System.Windows.Forms.Padding(2);
 			this.btnZaslon.Name = "btnZaslon";
 			this.btnZaslon.Size = new System.Drawing.Size(175, 63);
 			this.btnZaslon.TabIndex = 10;
 			this.btnZaslon.Text = "Početni zaslon";
 			this.btnZaslon.UseVisualStyleBackColor = false;
+			this.btnZaslon.Click += new System.EventHandler(this.btnZaslon_Click);
 			// 
 			// FrmGlavniIzbornik
 			// 
@@ -187,7 +193,7 @@
 			this.Controls.Add(this.splitterMenuScreen);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.IsMdiContainer = true;
-			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Margin = new System.Windows.Forms.Padding(2);
 			this.Name = "FrmGlavniIzbornik";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Wall@";
--- Wall@/PocetniZaslon/FrmGlavniIzbornik.cs
+++ Wall@/PocetniZaslon/FrmGlavniIzbornik.cs
@@ -17,18 +17,62 @@ namespace PocetniZaslon
             InitializeComponent();
         }
 
-        private void btnOdjava_Click(object sender, EventArgs e)
-        {
-            Application.Restart(); // Klikom na odjava aplikacija se resetira.
-        }
+		#region Upravljanje Gumbima Glavnog Izbornika
 
-		private void btnBankovniRacun_Click(object sender, EventArgs e) // Klikom na Bankovni racuni otvara se forma FrmBankovniRacuni.
+		/// <summary>
+		/// Metoda koja zatvara sve otvorene forme koje su otvorene unutar MDI containera forme "FrmGlavniIzbornik"
+		/// </summary>
+
+		private void UgasiSveOtvoreneProzore()
 		{
+			if (this.MdiChildren.Count<Form>() > 0) this.ActiveMdiChild.Close();
+		}
+		
+		private void btnBankovniRacun_Click(object sender, EventArgs e) // Klikom na Bankovni racuni otvara se forma FrmBankovniRacuni unutar MDI containera
+		{
+			UgasiSveOtvoreneProzore();
 			MDI_Forme.FrmBankovniRacuni formaBankovniRacun = new MDI_Forme.FrmBankovniRacuni();
 			formaBankovniRacun.MdiParent = this;
-			formaBankovniRacun.WindowState = FormWindowState.Maximized;
+			formaBankovniRacun.Dock = DockStyle.Fill;
 			formaBankovniRacun.Show();
 		}
+
+		private void btnZaslon_Click(object sender, EventArgs e)
+		{
+			UgasiSveOtvoreneProzore();
+		}
+
+		private void btnUnosTransakcije_Click(object sender, EventArgs e)
+		{
+			UgasiSveOtvoreneProzore();
+		}
+
+		private void btnPregled_Click(object sender, EventArgs e)
+		{
+			UgasiSveOtvoreneProzore();
+		}
+
+		private void btnInvesticijskiPortfolio_Click(object sender, EventArgs e)
+		{
+			UgasiSveOtvoreneProzore();
+		}
+
+		private void btnStatistika_Click(object sender, EventArgs e)
+		{
+			UgasiSveOtvoreneProzore();
+		}
+
+		private void btnPostavke_Click(object sender, EventArgs e)
+		{
+			UgasiSveOtvoreneProzore();
+		}
+
+		private void btnOdjava_Click(object sender, EventArgs e)
+		{
+			Application.Restart(); // Klikom na odjava aplikacija se resetira.
+		}
+
+		#endregion
 	}
 }
 
