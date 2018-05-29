503676f92f7e518573e40941d8018e0e1ffccb82
Filip Jovanović <filip.jovanovic14@gmail.com>
2018-05-30 00:13:18 +0200

    Dodane kontrole na FrmTransakcije
    Dodane labele i navigacijski gumbi za dodavanje transakcija (otvaraju nove forme).

--- Wall@/PocetniZaslon/MDI_Forme/FrmUnosTransakcija.Designer.cs
+++ Wall@/PocetniZaslon/MDI_Forme/FrmUnosTransakcija.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblNovaTransakcija = new System.Windows.Forms.Label();
+            this.lblOdabir = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNovaTransakcija
+            // 
+            this.lblNovaTransakcija.AutoSize = true;
+            this.lblNovaTransakcija.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblNovaTransakcija.Location = new System.Drawing.Point(300, 3);
+            this.lblNovaTransakcija.Name = "lblNovaTransakcija";
+            this.lblNovaTransakcija.Size = new System.Drawing.Size(501, 81);
+            this.lblNovaTransakcija.TabIndex = 0;
+            this.lblNovaTransakcija.Text = "Nova transakcija";
+            // 
+            // lblOdabir
+            // 
+            this.lblOdabir.AutoSize = true;
+            this.lblOdabir.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblOdabir.Location = new System.Drawing.Point(293, 179);
+            this.lblOdabir.Name = "lblOdabir";
+            this.lblOdabir.Size = new System.Drawing.Size(515, 54);
+            this.lblOdabir.TabIndex = 1;
+            this.lblOdabir.Text = "Odaberite vrstu transakcije:";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semilight", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(370, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(361, 89);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Trošak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semilight", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(370, 444);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(361, 89);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Prihod";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI Semilight", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(370, 586);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(361, 89);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Interni prijenos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // FrmUnosTransakcija
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1133, 713);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblOdabir);
+            this.Controls.Add(this.lblNovaTransakcija);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmUnosTransakcija";
             this.Text = "FrmUnosTransakcija";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNovaTransakcija;
+        private System.Windows.Forms.Label lblOdabir;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
