99247f27a2b3be81cb4de50137c446742e8c4f49
Juraj Cvetko <juraj.cvetko@hotmail.com>
2018-06-09 18:47:51 +0200

    Dodana metoda za dohvaćanje stanja dionice Apple-a

--- Wall@/DohvacanjePodatakaZaInvesticije/Avapi.Designer.cs
+++ Wall@/DohvacanjePodatakaZaInvesticije/Avapi.Designer.cs
@@ -36,32 +36,40 @@
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
+			this.txtSimbol2 = new System.Windows.Forms.TextBox();
+			this.txtDiT2 = new System.Windows.Forms.TextBox();
+			this.txtInfo2 = new System.Windows.Forms.TextBox();
+			this.txtOpen2 = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// txtSimbol
 			// 
-			this.txtSimbol.Location = new System.Drawing.Point(372, 65);
+			this.txtSimbol.Location = new System.Drawing.Point(149, 64);
 			this.txtSimbol.Name = "txtSimbol";
 			this.txtSimbol.Size = new System.Drawing.Size(190, 22);
 			this.txtSimbol.TabIndex = 0;
 			// 
 			// txtDiT
 			// 
-			this.txtDiT.Location = new System.Drawing.Point(372, 111);
+			this.txtDiT.Location = new System.Drawing.Point(149, 110);
 			this.txtDiT.Name = "txtDiT";
 			this.txtDiT.Size = new System.Drawing.Size(190, 22);
 			this.txtDiT.TabIndex = 0;
 			// 
 			// txtInfo
 			// 
-			this.txtInfo.Location = new System.Drawing.Point(372, 160);
+			this.txtInfo.Location = new System.Drawing.Point(149, 159);
 			this.txtInfo.Name = "txtInfo";
 			this.txtInfo.Size = new System.Drawing.Size(190, 22);
 			this.txtInfo.TabIndex = 0;
 			// 
 			// txtOpen
 			// 
-			this.txtOpen.Location = new System.Drawing.Point(372, 210);
+			this.txtOpen.Location = new System.Drawing.Point(149, 209);
 			this.txtOpen.Name = "txtOpen";
 			this.txtOpen.Size = new System.Drawing.Size(190, 22);
 			this.txtOpen.TabIndex = 0;
@@ -69,7 +77,7 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(285, 68);
+			this.label1.Location = new System.Drawing.Point(62, 67);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(54, 17);
 			this.label1.TabIndex = 1;
@@ -78,7 +86,7 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(285, 114);
+			this.label2.Location = new System.Drawing.Point(62, 113);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(78, 17);
 			this.label2.TabIndex = 1;
@@ -87,7 +95,7 @@
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(285, 163);
+			this.label3.Location = new System.Drawing.Point(62, 162);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(35, 17);
 			this.label3.TabIndex = 1;
@@ -96,27 +104,100 @@
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(251, 213);
+			this.label4.Location = new System.Drawing.Point(28, 212);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(112, 17);
 			this.label4.TabIndex = 1;
 			this.label4.Text = "Open vrijednost:";
+			// 
+			// txtSimbol2
+			// 
+			this.txtSimbol2.Location = new System.Drawing.Point(531, 67);
+			this.txtSimbol2.Name = "txtSimbol2";
+			this.txtSimbol2.Size = new System.Drawing.Size(190, 22);
+			this.txtSimbol2.TabIndex = 0;
+			// 
+			// txtDiT2
+			// 
+			this.txtDiT2.Location = new System.Drawing.Point(531, 113);
+			this.txtDiT2.Name = "txtDiT2";
+			this.txtDiT2.Size = new System.Drawing.Size(190, 22);
+			this.txtDiT2.TabIndex = 0;
+			// 
+			// txtInfo2
+			// 
+			this.txtInfo2.Location = new System.Drawing.Point(531, 162);
+			this.txtInfo2.Name = "txtInfo2";
+			this.txtInfo2.Size = new System.Drawing.Size(190, 22);
+			this.txtInfo2.TabIndex = 0;
+			// 
+			// txtOpen2
+			// 
+			this.txtOpen2.Location = new System.Drawing.Point(531, 212);
+			this.txtOpen2.Name = "txtOpen2";
+			this.txtOpen2.Size = new System.Drawing.Size(190, 22);
+			this.txtOpen2.TabIndex = 0;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(444, 70);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(54, 17);
+			this.label9.TabIndex = 1;
+			this.label9.Text = "Simbol:";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(444, 116);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(78, 17);
+			this.label10.TabIndex = 1;
+			this.label10.Text = "Time i date";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(444, 165);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(35, 17);
+			this.label11.TabIndex = 1;
+			this.label11.Text = "Info:";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(410, 215);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(112, 17);
+			this.label12.TabIndex = 1;
+			this.label12.Text = "Open vrijednost:";
 			// 
 			// Avapi
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label12);
 			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label11);
 			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label10);
 			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label9);
 			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtOpen2);
 			this.Controls.Add(this.txtOpen);
+			this.Controls.Add(this.txtInfo2);
 			this.Controls.Add(this.txtInfo);
+			this.Controls.Add(this.txtDiT2);
 			this.Controls.Add(this.txtDiT);
+			this.Controls.Add(this.txtSimbol2);
 			this.Controls.Add(this.txtSimbol);
 			this.Name = "Avapi";
 			this.Text = "Avapi";
+			this.Load += new System.EventHandler(this.Avapi_Load);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -132,5 +213,13 @@
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox txtSimbol2;
+		private System.Windows.Forms.TextBox txtDiT2;
+		private System.Windows.Forms.TextBox txtInfo2;
+		private System.Windows.Forms.TextBox txtOpen2;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label12;
 	}
 }
--- Wall@/DohvacanjePodatakaZaInvesticije/Avapi.cs
+++ Wall@/DohvacanjePodatakaZaInvesticije/Avapi.cs
@@ -21,12 +21,20 @@ namespace DohvacanjePodatakaZaInvesticije
 		{
 			InitializeComponent();
 			MSFT();
+			AAPL();
 		}
-		private void MSFT()
+
+		public IAvapiConnection Konekcija()
 		{
 			IAvapiConnection connection = AvapiConnection.Instance;
 			connection.Connect("EP6HXRE6I90RNOEQ");
 
+			return connection;
+		}
+		public void MSFT()
+		{
+			IAvapiConnection connection = Konekcija();
+			
 			Int_TIME_SERIES_DAILY tIME_SERIES_DAILY = connection.GetQueryObject_TIME_SERIES_DAILY();
 			IAvapiResponse_TIME_SERIES_DAILY avapiResponse = tIME_SERIES_DAILY.Query("MSFT", Const_TIME_SERIES_DAILY.TIME_SERIES_DAILY_outputsize.compact);
 
@@ -44,10 +52,39 @@ namespace DohvacanjePodatakaZaInvesticije
 				{
 					txtOpen.Text = timeseries.open;
 				}
-				
 			};
 			
 
+
+		}
+		public void AAPL()
+		{
+			Konekcija();
+			IAvapiConnection connection = Konekcija();
+
+			Int_TIME_SERIES_DAILY tIME_SERIES_DAILY = connection.GetQueryObject_TIME_SERIES_DAILY();
+			IAvapiResponse_TIME_SERIES_DAILY avapiResponse = tIME_SERIES_DAILY.Query("AAPL", Const_TIME_SERIES_DAILY.TIME_SERIES_DAILY_outputsize.compact);
+
+			var data = avapiResponse.Data;
+			if (data.Error)
+			{
+				MessageBox.Show("Došlo je do problema s dohvaćanjem podataka");
+			}
+			else
+			{
+				txtSimbol2.Text = data.MetaData.Symbol;
+				txtDiT2.Text = data.MetaData.TimeZone;
+				txtInfo2.Text = data.MetaData.Information;
+				foreach (var timeseries in data.TimeSeries)
+				{
+					txtOpen2.Text = timeseries.open;
+				}
+
+
+			}
+		}
+			private void Avapi_Load(object sender, EventArgs e)
+		{
 
 		}
 	}
