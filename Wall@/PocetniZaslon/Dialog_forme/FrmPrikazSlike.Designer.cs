76fda9419b7c9919da16e22c916bb20bad7dfe90
Luka Čosić <cosic.luka1@gmail.com>
2018-06-24 17:17:24 +0200

    Postavljeno otvaranje slike računa.
    Ako slika postoji na spremljenoj lokaciji računala na kojem je pokrenuta aplikacija otvara se slika unutar picturebox-a forme FrmPrikazSlike, a ako ne postoji onda se prikazuje poruka o nepostojanju slike na zapisanoj lokaciji.
    Također postavljeno je da se forma prilagođava veličini slike.

--- Wall@/PocetniZaslon/Dialog_forme/FrmPrikazSlike.Designer.cs
+++ Wall@/PocetniZaslon/Dialog_forme/FrmPrikazSlike.Designer.cs
@@ -28,12 +28,47 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.pictureBoxSlika = new System.Windows.Forms.PictureBox();
+			this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxSlika)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBoxSlika
+			// 
+			this.pictureBoxSlika.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBoxSlika.Location = new System.Drawing.Point(0, 0);
+			this.pictureBoxSlika.Name = "pictureBoxSlika";
+			this.pictureBoxSlika.Size = new System.Drawing.Size(997, 534);
+			this.pictureBoxSlika.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBoxSlika.TabIndex = 0;
+			this.pictureBoxSlika.TabStop = false;
+			// 
+			// fileSystemWatcher1
+			// 
+			this.fileSystemWatcher1.EnableRaisingEvents = true;
+			this.fileSystemWatcher1.SynchronizingObject = this;
+			// 
+			// FrmPrikazSlike
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "FrmPrikazSlike";
+			this.ClientSize = new System.Drawing.Size(997, 534);
+			this.Controls.Add(this.pictureBoxSlika);
+			this.Name = "FrmPrikazSlike";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Slika računa";
+			this.Load += new System.EventHandler(this.FrmPrikazSlike_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxSlika)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBoxSlika;
+		private System.IO.FileSystemWatcher fileSystemWatcher1;
 	}
 }
--- Wall@/PocetniZaslon/Dialog_forme/FrmPrikazSlike.cs
+++ Wall@/PocetniZaslon/Dialog_forme/FrmPrikazSlike.cs
@@ -12,9 +12,26 @@ namespace PocetniZaslon.Dialog_forme
 {
 	public partial class FrmPrikazSlike : Form
 	{
-		public FrmPrikazSlike()
+		public string LokacijaSlike { get; set; }
+		public FrmPrikazSlike(string text)
 		{
 			InitializeComponent();
+			LokacijaSlike = text;
+		}
+
+		private void FrmPrikazSlike_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				pictureBoxSlika.Image = Image.FromFile(LokacijaSlike);
+				this.Size = new Size(pictureBoxSlika.Image.Width + 15, pictureBoxSlika.Image.Height + 40);
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Slika ne postoji na zapisanoj lokaciji.");
+				this.Close();
+			}
+			
 		}
 	}
 }
